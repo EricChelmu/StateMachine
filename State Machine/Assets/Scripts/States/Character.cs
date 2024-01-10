f5fab425b82d1a731a9b22346f9290a0035c8941
Eric Chelmu <53497444+EricChelmu@users.noreply.github.com>
2024-01-10 15:45:19 +0100

    removed cinemachine, fixed animations

--- State Machine/Assets/Scripts/States/Character.cs
+++ State Machine/Assets/Scripts/States/Character.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -50,7 +51,6 @@ namespace Player
         public Animator animator;
         [HideInInspector]
         public Vector3 playerVelocity;
-
 
         // Start is called before the first frame update
         private void Start()
