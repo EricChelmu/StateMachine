d464e574145545a14748199123d0915f0551873c
Eric Chelmu <eric.chelmu12@gmail.com>
2025-01-17 13:20:05 +0100

    added loot and npc interaction

--- State Machine/Assets/Scripts/InteractionSystem/InteractableLoot.cs
+++ State Machine/Assets/Scripts/InteractionSystem/InteractableLoot.cs
@@ -1,18 +1,61 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableLoot : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InteractableLoot : Interactable
     {
-        
-    }
+        public override void Start()
+        {
+            base.Start();
+        }
+        protected override void Interaction()
+        {
+            base.Interaction();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            print("Unfortunately you can't take my loot yet.");
+            DisableCollider();
+            Destroy(this);
+
+            //OpenLoot();
+        }
+        void OpenLoot()
+        {
+            //GetComponent<ItemContainer>().SetLoot();
+            //Events.ShowLoot(true);
+
+            //Events.OnShowLoot += TriggerLoot;
+        }
+        void TriggerLoot(bool openLoot)
+        {
+            if (openLoot)
+            {
+                //GetComponent<ItemContainer>().SetLoot();
+                //Events.ShowLoot(true);
+            }
+            else
+            {
+                //GetComponent<ItemContainer>().ResetLoot();
+                //Events.OnShowLoot -= TriggerLoot;
+            }
+        }
+
+        void DisableCollider()
+        {
+            if (TryGetComponent(out BoxCollider boxCollider))
+            {
+                boxCollider.enabled = false;
+            }
+            else if (TryGetComponent(out CapsuleCollider capsCollider))
+            {
+                capsCollider.enabled = false;
+            }
+            else
+            {
+                print("Error, no collider found!");
+            }
+        }
     }
 }
